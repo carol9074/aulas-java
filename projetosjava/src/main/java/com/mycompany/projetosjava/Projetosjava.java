bb3ec1da84536ca57785b41fe6c2c7fb887324be
Carol Pereira <carolainelopespereira9074@gmail.com>
2023-05-02 20:03:49 -0300

    cod bancario coma funcao JOptionpane.

--- projetosjava/src/main/java/com/mycompany/projetosjava/Projetosjava.java
+++ projetosjava/src/main/java/com/mycompany/projetosjava/Projetosjava.java
@@ -1,44 +1,37 @@
 package com.mycompany.projetosjava;
 
+import java.awt.Component;
 import java.util.Arrays;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Projetosjava {
 
     public static String[] cadastro() {
-        Scanner t = new Scanner(System.in);
-        Scanner s = new Scanner(System.in);
-
         String dados[] = new String[10];
         int op = 0;
         long num = 0;
+        Component parentComponent;
 
-        System.out.println("Informe seu nome:");
-        dados[0] = t.nextLine();
+        JOptionPane.showMessageDialog(null, "Bem vindo ao cadastro de usuario!");
+        dados[0] = JOptionPane.showInputDialog("Informe seu nome:");
 
         while (op != 2) {
-            System.out.println("Informe seu cpf:");
-            dados[1] = s.nextLine();
+            dados[1] = JOptionPane.showInputDialog("Informe seu CPF:");
 
             if (dados[1].length() == 11) {
                 op = 2;
 
                 try {
                     num = Long.parseLong(dados[1]);
-                    System.out.println("\nCPF Valido\n" + dados[1]);
+                    JOptionPane.showMessageDialog(null, "CPF valido");
                 } catch (NumberFormatException e) {
-                    System.out.println("\nCPF inválido\n" + dados[1]);
+                    JOptionPane.showMessageDialog(null, "CPF inválido");
                     op = 1;
                 }
             }
         }
-        System.out.println("Informe seu telefone:");
-        dados[2] = s.nextLine();
-
-        System.out.println(dados[0]);
-        System.out.println(dados[1]);
-        System.out.println(dados[2]);
-
+        dados[2] = JOptionPane.showInputDialog("Informe seu telefone:");
         return dados;
     }
 
@@ -54,20 +47,17 @@ public class Projetosjava {
     }
 
     public static int searchName(String[] nomes) {
-
         int pos = 0;
         String nameSearch;
-        Scanner t = new Scanner(System.in);
 
-        System.out.println("informe o nome para busca: ");
-        nameSearch = t.nextLine();
+        nameSearch = JOptionPane.showInputDialog("Informe o nome para busca:");
 
         for (int i = 0; i < nomes.length; i++) {
             if (nomes[i].equals(nameSearch)) {
                 pos = i;
                 return pos;
             } else {
-                System.out.println("Não a cadrastro");
+                JOptionPane.showMessageDialog(null, "Não possui cadastro!");
             }
         }
         return 15;
@@ -75,28 +65,21 @@ public class Projetosjava {
 
     public static int searchCpf(String[] cpfs) {
         int pos;
-        String Cpfsearch;
-  
-        Scanner s = new Scanner(System.in);
-        System.out.println("informe o CPF para busca: ");
-        String CpfSearch = s.nextLine();
+
+        String CpfSearch = JOptionPane.showInputDialog("Informe o nome para busca:");
 
         for (int i = 0; i < cpfs.length; i++) {
             if (cpfs[i].equals(CpfSearch)) {
                 pos = i;
-                return pos;              
+                return pos;
             } else {
-                 System.out.println("Não a cadrastro");
+                JOptionPane.showMessageDialog(null, "Não possui cadastro!");
             }
         }
         return 15;
     }
 
     public static void main(String[] args) {
-
-        Scanner t = new Scanner(System.in);
-        Scanner s = new Scanner(System.in);
-
         String nome[] = new String[10];
         float valor[] = new float[10];
         String cell[] = new String[10];
@@ -104,98 +87,91 @@ public class Projetosjava {
         String cliente[] = new String[2];
         float saque;
         int pos = 0;
-        int op ;
+        int op;
+        float aux = 0;
 
         do {
-            System.out.println(" 1-Cadastrar \n 2-Vizualizar Perfil \n 3-Debito \n 4-Credito \n 5-Saldo \n 6-Atualizar \n 0-Sair \n");
-            op = s.nextInt();
-
+            JOptionPane.showMessageDialog(null, "Escolha uma das opções abaixo.");
+            op = Integer.parseInt(JOptionPane.showInputDialog("1-Cadastrar \n 2-Vizualizar Perfil\n 3-Debito \n 4-Credito \n 5-Saldo \n 6-Atualizar \n 0-Sair "));
             switch (op) {
                 case 1 -> {
-                    System.out.println("---- Bem vindo ao cadrastro ---- \nPrecisamos que preencha algumas informações,logo abaixo.");
                     pos = vereficacao(nome);
                     if (pos == 15) {
-                        System.out.println("Não temos vagas.");
+                        JOptionPane.showMessageDialog(null, "Não temos vagas.");
                     } else {
                         cliente = cadastro();
                         nome[pos] = cliente[0];
                         cpf[pos] = cliente[1];
                         cell[pos] = cliente[2];
                         valor[pos] = 0;
-                        System.out.println("\nSeu cadastro está concluido com sucesso!");
-                        System.out.println("nome:" + nome[pos]);
-                        System.out.println("cpf:" + cpf[pos]);
-                        System.out.println("cell:" + cell[pos]);
-
+                        JOptionPane.showMessageDialog(null, "\nSeu cadastro está concluido com sucesso!.");
+                        JOptionPane.showMessageDialog(null, "Nome:" + nome[pos]);
+                        JOptionPane.showMessageDialog(null, "CPF:" + cpf[pos]);
+                        JOptionPane.showMessageDialog(null, "Celular:" + cell[pos]);
                     }
                     break;
                 }
                 case 2 -> {
-                    System.out.println("1-Buscar nome: \n 2-Buscar CPF:");
-                    op = s.nextInt();
+                    op = Integer.parseInt(JOptionPane.showInputDialog("1-Buscar nome: \\n 2-Buscar CPF:"));
 
                     if (op == 1) {
                         pos = searchName(nome);
                         if (pos == 15) {
-                            System.out.println("Não cadastrado.");
+                            JOptionPane.showMessageDialog(null, "Não  cadastrado.");
                         } else {
-                            System.out.println("nome:" + nome[pos]);
-                            System.out.println("cpf:" + cpf[pos]);
-                            System.out.println("cell:" + cell[pos]);
-                            System.out.println("saldo:" + valor[pos]);
+                            JOptionPane.showMessageDialog(null, "Nome:" + nome[pos]);
+                            JOptionPane.showMessageDialog(null, "CPF:" + cpf[pos]);
+                            JOptionPane.showMessageDialog(null, "Celular:" + cell[pos]);
+                            JOptionPane.showMessageDialog(null, "Saldo" + valor[pos]);
+
                         }
-                    }
-                    else {
+                    } else {
                         pos = searchCpf(cpf);
                         if (pos == 15) {
-                            System.out.println("Não cadastrado.");
+                            JOptionPane.showMessageDialog(null, "Não  cadastrado.");
                         } else {
-                            System.out.println("nome:" + nome[pos]);
-                            System.out.println("cpf:" + cpf[pos]);
-                            System.out.println("cell:" + cell[pos]);
-                            System.out.println("saldo:" + valor[pos]);
+                            JOptionPane.showMessageDialog(null, "Nome:" + nome[pos]);
+                            JOptionPane.showMessageDialog(null, "CPF:" + cpf[pos]);
+                            JOptionPane.showMessageDialog(null, "Celular:" + cell[pos]);
+                            JOptionPane.showMessageDialog(null, "Saldo" + valor[pos]);
+
                         }
-                }
+                    }
                     break;
                 }
                 case 3 -> {
                     pos = searchName(nome);
-                    System.out.println("Saldo Atual:" + valor[pos]);
-                    System.out.println("Informe o que deseja sacar:");
-                    saque = s.nextFloat();
+                    JOptionPane.showMessageDialog(null, "Saldo atual:" + valor[pos]);
+                    saque = Float.parseFloat(JOptionPane.showInputDialog("Informe o que deseja sacar:"));
                     if (valor[pos] >= saque) {
                         valor[pos] = valor[pos] - saque;
-                        System.out.println("Saque:" + saque);
-                        System.out.println("Saldo:" + valor[pos]);
+                        JOptionPane.showMessageDialog(null, "Saque:" + saque);
+                        JOptionPane.showMessageDialog(null, "Saldo:" + valor[pos]);
                     } else {
-                        System.out.println("Saldo insuficiente\n");
+                        JOptionPane.showMessageDialog(null, "Saldo insuficiente.");
                     }
                     break;
                 }
                 case 4 -> {
                     pos = searchName(nome);
-                    System.out.println("Informe o valor de deposito:");
-                    valor[pos] = valor[pos] + s.nextFloat();
+                    aux = Float.parseFloat(JOptionPane.showInputDialog("Informe o valor de deposito:"));
+                    valor[pos] = valor[pos]+aux;                   
                     break;
                 }
                 case 5 -> {
                     pos = searchName(nome);
-                    System.out.println("saldo:" + valor[pos]);
+                    JOptionPane.showMessageDialog(null, "Saldo:" + valor[pos]);
                     break;
                 }
                 case 6 -> {
                     pos = searchName(nome);
-                    System.out.println("Cell atual:" + cell[pos]);
-                    cell[pos] = s.nextLine();
-                    System.out.println("Informe o novo cell:");
-                    cell[pos] = s.nextLine();
+                    JOptionPane.showMessageDialog(null, "Cell atual:" + cell[pos]);
+                    cell[pos] = JOptionPane.showInputDialog("Informe o novo cell:");
+                    JOptionPane.showMessageDialog(null, "Cell novo:" + cell[pos]);
                     break;
                 }
             }
         } while (op != 0);
     }
 
-    private static int CpfSearch(String[] cpf) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 }
